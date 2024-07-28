14ddacb389fcaa36dbf375be520b254ce0a80f03
Swastik Behera <swastikb97@gmail.com>
2024-07-29 01:37:41 +0530

    Tasks are added

---
+++ LGMVIP-Web-Task-5/admin/aboutus.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title>About</title>
+<link rel="stylesheet" href="../csss/aboutus.css" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+
+</head>
+<body>
+    <header>
+      <nav>
+        <div class="row clearfix">
+            <ul class="main-nav" animate slideInDown>
+                <li><a href="../index.php">HOME</a></li>
+                <li><a href="aboutus.php">ABOUT</a></li>
+                <li><a href="contactus.php">CONTACT</a></li>
+                <li><a href="../login.php">ADMIN LOGIN</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="main-content-header">
+          <h2 class="search">About Us</h2>
+             <div class="div2">
+             <h3>Student Result Management System</h2>
+             <p>Welcome to the Student Result Management System (SRMS), a cutting-edge platform designed to streamline and enhance the process of managing student results. Our mission is to provide educational institutions with a robust, user-friendly, and efficient solution for handling academic records, ensuring accuracy, and facilitating better communication between students, teachers, and administrators.</p>
+             <p> Student Result Management System is a technological opportunity for the school, college university and coaching centre institutions searching for a secure, simple and alternative solution to the conventional paper-based exam results  evaluation, reporting and distribution.</p>
+             </div>  
+          </div>
+    </header>
+    
+</body>
+</html>
